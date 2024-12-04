ec2a8f75344fcb850f224c62d15775a60b7c1ba4
luchernan <148318614+luchernan@users.noreply.github.com>
2024-12-04 13:56:12 +0100

    FINAL

--- app.js
+++ app.js
@@ -1,18 +1,127 @@
-const clientId = '22764d228fb54fdfa21eb2a61587689a';
-//const clientIdSecret = '25ee73e99a014fc69d5d04f578622bbf';
+let musics = [];
+let id;
+let favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
-const redirectUri = 'http://localhost:3000'; // Cambia a tu URL
-const scopes = [
-    'user-read-private',
-    'user-read-email',
-    'playlist-read-private',
-];
+document.querySelector("#search").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    Search();
+  }
+});
 
-// Redirige al usuario a la página de autenticación de Spotify
-function authenticateWithSpotify() {
-    const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${clientId}&scope=${scopes.join('%20')}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-    window.location.href = authUrl;
+document.querySelector("#buttonSearch").addEventListener("click", Search);
+
+function Search() {
+  document.querySelectorAll("#results > div").forEach((div) => div.remove());
+  const music = document.querySelector("#search").value;
+
+  const url = `https://deezerdevs-deezer.p.rapidapi.com/search?q=${music}`;
+  const headers = {
+    "X-RapidAPI-Key": "3d18fadb96mshfdcba924e6d39f5p136872jsn47e833abda1a",
+    "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
+  };
+
+  fetch(url, { method: "GET", headers })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const results = data.data;
+
+      if (!results || results.length === 0) {
+        const noResults = document.createElement("div");
+        noResults.className = "noResults";
+        noResults.textContent = "Ningún resultado encontrado";
+        document.querySelector("#results").appendChild(noResults);
+      } else {
+        const setResult = new Set();
+        const filterResults = results.filter((result) => {
+          const duplicated = setResult.has(result.title);
+          setResult.add(result.title);
+          return !duplicated;
+        });
+
+        filterResults.forEach((music, index) => {
+          musics[index] = music;
+          const resultDiv = document.createElement("div");
+          resultDiv.id = music.id;
+          resultDiv.className = "result";
+          resultDiv.innerHTML = `
+                        <img src="${music.artist.picture}" alt="Imagen del artista">
+                        <h3 id="title${index}" class="title">${music.title}</h3>
+                        <div class="autor">${music.artist.name}</div>
+                        <div class="fav">
+                            <button onclick="addToFavorites(${index})">Add to favourites</button>
+                        </div>
+                        <div id="content${music.id}" class="content"></div>
+                    `;
+          document.querySelector("#results").appendChild(resultDiv);
+
+          resultDiv.addEventListener("click", function () {
+            document
+              .querySelectorAll(".content")
+              .forEach((content) => (content.innerHTML = ""));
+            document.querySelector("#selected")?.setAttribute("id", id);
+            id = this.id;
+            this.id = "selected";
+            const selectedMusic = musics.find((m) => m.id === parseInt(id));
+            const contentDiv = document.querySelector(`#content${id}`);
+            contentDiv.innerHTML = `
+                            <audio src="${selectedMusic.preview}" controls></audio>`;
+          });
+        });
+      }
+    })
+    .catch((error) => console.error(error));
 }
 
-// Llama a esta función en un botón o evento específico
-document.getElementById('loginButton').addEventListener('click', authenticateWithSpotify);
+function addToFavorites(index) {
+  const music = musics[index];
+  if (!favorites.some((fav) => fav.id === music.id)) {
+    favorites.push(music);
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+    renderFavorites();
+  }
+}
+
+function removeFromFavorites(index) {
+  favorites.splice(index, 1);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+  renderFavorites();
+}
+
+function renderFavorites() {
+  const favoriteResults = document.querySelector("#favoriteResults");
+  favoriteResults.innerHTML = "";
+
+  if (favorites.length === 0) {
+    const noFavoritesMessage = document.createElement("h3");
+    noFavoritesMessage.className = "no-favorites-message";
+    noFavoritesMessage.textContent = "You do not have favourites yet";
+    document.querySelector("#favoriteResults").appendChild(noFavoritesMessage);
+  } else {
+    const noFavoritesMessage = document.querySelector(".no-favorites-message");
+    if (noFavoritesMessage) {
+      noFavoritesMessage.remove();
+    }
+
+    favorites.forEach((music, index) => {
+      const favoriteDiv = document.createElement("div");
+      favoriteDiv.className = "favorite-song";
+      favoriteDiv.innerHTML = `
+        <img src="${music.artist.picture}" alt="Imagen del artista">
+        <h3 class="title">${music.title}</h3>
+        <div class="autor">${music.artist.name}</div>
+        <audio src="${music.preview}" controls></audio>
+        <div class="fav">
+          <button onclick="removeFromFavorites(${index})">Remove from favourites</button>
+        </div>
+      `;
+      favoriteResults.appendChild(favoriteDiv);
+    });
+  }
+}
+
+renderFavorites();
